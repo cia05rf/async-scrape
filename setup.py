8f864dbfdcb1b9439ece6e8817b0a864081cb909
Robert Franklin <robert.franklin@boots.co.uk>
2021-08-31 15:07:08 +0100

    init

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from distutils.core import setup
 setup(
-  name = 'async-scrape',         # How you named your package folder (MyLib)
+  name = 'async-scrape',
   packages = [
     'async_scrape',
     'async_scrape/libs'
-    ],   # Chose the same as "name"
-  version = '0.1.3',      # Start with a small number and increase it with every change you make
+    ],
+  version = '0.1.3',
   license = 'MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A package designed to scrape webpages using aiohttp and asyncio. Has some error handling to overcome common issues such as sites blocking you after n requests over a short period.',   # Give a short description about your library
-  author = 'Robert Franklin',                   # Type in your name
-  author_email = 'cia05rf@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/cia05rf/async-scrape/',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/cia05rf/async-scrape/archive/refs/tags/v0.1.0.tar.gz',    # I explain this later on
-  keywords = [],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
+  author = 'Robert Franklin',
+  author_email = 'cia05rf@gmail.com',
+  url = 'https://github.com/cia05rf/async-scrape/',
+  download_url = 'https://github.com/cia05rf/async-scrape/archive/refs/tags/v0.1.0.tar.gz',
+  keywords = [],
+  install_requires=[
         "asyncio",
         "nest-asyncio",
         "aiohttp",
@@ -25,7 +25,7 @@ setup(
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3.9',      #Specify which pyhton versions that you want to support
+    'License :: OSI Approved :: MIT License',   # Pick a license
+    'Programming Language :: Python :: 3.9',      # Specify which pyhton versions that you want to support
   ],
 )
