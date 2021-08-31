e3c18702f4721d6fddf8117ce387ae7061454f4a
Robert Franklin <robert.franklin@boots.co.uk>
2021-08-31 15:05:08 +0100

    init

--- setup.py
+++ setup.py
@@ -18,6 +18,8 @@ setup(
         "nest-asyncio",
         "aiohttp",
         "PyPAC",
+        "requests-html",
+        "pandas"
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
