1fd1f049220ddf61153b053d6faaeb4252b098c0
Robert Franklin <cia05rf@gmail.com>
2022-03-20 21:01:40 +0000

    changed __init__ of tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,7 +2,8 @@ import os
 import json
 
 #Create env variables
-with open("env.json", "r") as f:
-    envs = json.loads(f.read())
-    for env, var in envs.items():
-        os.environ[env] = var
+if os.path.exists("env.json"):
+    with open("env.json", "r") as f:
+        envs = json.loads(f.read())
+        for env, var in envs.items():
+            os.environ[env] = var
